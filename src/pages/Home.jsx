ac329c371c3e70f5fbde4648fbf45acc07a557ae
Adnan <mohdadnan.code@gmail.com>
2025-12-08 17:02:57 +0530

    home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,31 @@
-import React from 'react'
+import HeroSection from '../components/HeroSection';
+import ProductHighlights from '../components/ProductHighlights';
+import CategoriesSection from '../components/CategoriesSection';
+import BrandStrip from '../components/BrandStrip';
+import RevealOnScroll from '../components/RevealOnScroll';
 
-export const Home = () => {
+const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <div className="space-y-20">
+
+      <RevealOnScroll >
+      <HeroSection />
+      </RevealOnScroll>
+
+      <RevealOnScroll delay={0.1}>
+      <ProductHighlights />
+      </RevealOnScroll>
+
+      <RevealOnScroll delay={0.2}>
+      <CategoriesSection />
+      </RevealOnScroll>
+
+      <RevealOnScroll delay={0.3}>
+      <BrandStrip />
+      </RevealOnScroll>
+      
+    </div>
+  );
+};
+
+export default Home;
